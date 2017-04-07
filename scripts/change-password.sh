9642f8dcb6a103cb65fad6c4812cccc9091c870b
“sstone72389” <sstone72389@gmail.com>
2017-04-07 16:27:50 -0400

    Adds ability to change password
    Adds ability to change password. New Form elements on HTML file added.
    JS added to mimic curl request. Accesses specific token for use of each
    individual user.

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-#curl "http://localhost:3000/change-password/${ID}" \
-curl "http://httpbin.org/patch?id=${ID}" \
-  --include \
-  --request PATCH \
-  --data-urlencode ""
+curl --include --request PATCH http://localhost:4741/change-password/$ID \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "12345",
+      "new": "super sekrit"
+    }
+  }'
 
 # data output from curl doesn't have a trailing newline
 echo
